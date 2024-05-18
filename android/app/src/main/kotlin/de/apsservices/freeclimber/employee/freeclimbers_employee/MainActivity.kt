0182b84b10904a9217160498d4a859e4d9b074d4
PandasCodi <61013205+Majestr32@users.noreply.github.com>
2024-05-18 12:04:16 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/de/apsservices/freeclimber/employee/freeclimbers_employee/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.apsservices.freeclimber.employee.freeclimbers_employee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:freeclimbers_employee/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
